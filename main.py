cbcb5883db04fce5f0d66382f7caf7b7b05d7872
Sourav Singh <67225907+trailblazer-99@users.noreply.github.com>
2024-08-01 12:30:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,5 @@ async def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host = "localhost", port = 6500)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
